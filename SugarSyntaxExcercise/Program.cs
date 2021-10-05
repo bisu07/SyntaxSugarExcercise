e68fc90c1a483c76a86fb113a8f72160d314ee2a
bisu07 <g26.bishal@gmail.com>
2021-10-04 22:16:21 -0500

    this is initial commit

---
+++ SugarSyntaxExcercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Transactions;
+
+namespace SugarSyntaxExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the answer :");
+            var answer = Convert.ToInt32(Console.ReadLine());
+
+
+            var response  = (answer < 9)?$"{answer} is less than nine":
+             $"{answer} is greater than or equal to  9";
+            Console.WriteLine(response);
+
+        }
+    }
+}
